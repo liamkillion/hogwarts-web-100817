e5500939d612d336f42a301ec833f4cc03c00784
Liam Killion <liam.killion@gmail.com>
2017-12-14 16:54:53 -0500

    pig stuff

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ class App extends Component {
     super();
     this.state = {
       pigs: [],
-      pigFilter: false
+      pigFilter: false,
+      porkSort: ""
     }
   }
 
@@ -26,14 +27,40 @@ class App extends Component {
     this.setState({pigFilter: !this.state.pigFilter})
   }
 
+  handleSortClick = (event) => {
+    this.setState({porkSort: event.target.value})
+  }
+
+  pigSorter = (pig1, pig2) => {
+    piggies = () => {if (this.state.porkSort === "name") {
+      var pig1 = pig1.name.toUpperCase(); // ignore upper and lowercase
+      var pig2 = pig2.name.toUpperCase(); // ignore upper and lowercase
+        if (pig1 < pig2) {
+          return -1;
+        }
+        if (pig1 > pig2) {
+          return 1;
+        }
+    } else {
+      return pig2.fridgeWeight - pig1.fridgeWeight
+    }
+  }
+    this.state.pigsFilter ? piggies.filter(pig => pig.greased) : piggies
+  }
+
+
   render() {
-    var pigFiltering = this.state.pigFilter ? this.state.pigs.filter(pig => pig.greased) : this.state.pigs
-    debugger;
+    const pigFiltering = [...this.state.pigs].sort(this.pigSorter)
+    console.log(pigFiltering)
     return (
       <div className="App">
         < Nav />
-      <button onClick={this.handleClick}>Greased?</button>
-        <Piglist pigs={this.pigFiltering}/>
+        <button onClick={this.handleClick}>Greased?</button>
+        <select onChange={this.handleSortClick}>Sort By Weight?
+          <option value="weight">Weight</option>
+          <option value="name">Name</option>
+        </select>
+        <Piglist pigs={pigFiltering}/>
       </div>
     )
   }
