92719a6a586fd019d4184e9910003569e7f2978e
github username <github email address>
2017-12-14 16:57:20 -0500

    no changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
+import Pigs from '/Users/Mimi/Development/code/hogwarts-web-100817/src/porkers_data.js'
 
 class App extends Component {
   render() {
